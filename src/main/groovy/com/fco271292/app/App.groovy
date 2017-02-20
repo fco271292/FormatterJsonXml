d9793fcdde037c164577810a4f045c3a53e4a216
Francisco Reyes <fco271292@gmail.com>
2017-02-20 15:47:14 -0600

    Se ajusta uso de objetos, asi como manejo del contenido del archivo

--- src/main/groovy/com/fco271292/app/App.groovy
+++ src/main/groovy/com/fco271292/app/App.groovy
@@ -8,17 +8,23 @@ class App {
 
 	static main(args) {
 		
-		String pathFile = "C:\\Users\\Gigabyte\\Documents\\workspace\\FormatterJsonXml\\src\\test\\groovy\\source\\jsonTest.json"
-		def fileTest = new File(pathFile)
-		def contentFile = new FileManager().readFile(fileTest.getPath())
-		String headerJSON = new FormatterJSON().getHeaderJSON(contentFile)
-		String bodyJSON = new FormatterJSON().getbodyJSON(contentFile)
-		println "Header ${headerJSON} \n Body ${bodyJSON}"
+		FormatterJSON formatterJSON = new FormatterJSON()
+		FileManager fileManager  = new FileManager()
+		File file = new File("")
+		
+		String pathFile = "${file.absolutePath}\\src\\test\\groovy\\source\\jsonTest.json"
+		//def fileTest = new File(pathFile)
+		def contentFile =fileManager.readFile(pathFile)
+		String contentFileString = contentFile.join("\n")
+		String headerJSON = formatterJSON.getHeaderJSON(contentFileString)
+		String bodyJSON = formatterJSON.getbodyJSON(contentFileString)
+		//println "Header ${headerJSON} \n Body ${bodyJSON}"
 		def jsonCommand = new JSONCommand(header: headerJSON,body: bodyJSON )
-		//new FileManager().createFile(jsonCommand)
+		new FileManager().createFile(jsonCommand)
 		println  "FIN..."
 		//new FormatterJSON().formatterJSONPretty(bodyJSON)
 		//new FormatterJSON().validateJSONInput(jsonInput)
+		
 	}
 
 }
