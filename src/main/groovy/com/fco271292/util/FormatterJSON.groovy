f67d65e85759b6e2b809df916a14bbf2ccb83036
fco271292 <juan271292@hotmail.com>
2017-02-21 09:57:26 -0600

    Se agrega metodo para ajustar JSON

--- src/main/groovy/com/fco271292/util/FormatterJSON.groovy
+++ src/main/groovy/com/fco271292/util/FormatterJSON.groovy
@@ -5,16 +5,9 @@ import groovy.json.JsonOutput
 class FormatterJSON {
 	
 	String formatterJSONPretty(String inputJSON){
+		
 		String json = JsonOutput.prettyPrint(inputJSON)
-		//println "JSON ${json}"
-	}
-	
-	def validateJSONInput(List<String> inputJSON){
-		//println inputJSON
-		String firtsParenthesis = inputJSON.find{character->
-			character.contains('{')
-		}
-		println "Encontro ${firtsParenthesis}"
+		
 	}
 	
 	String getHeaderJSON(String inputJSON){
@@ -28,12 +21,16 @@ class FormatterJSON {
 	}
 	
 	String getbodyJSON(String inputJSON){
+		
 		String bodyJSON = ""
 		Integer indexOfParenthesis = inputJSON.toString().indexOf("{")
 		Integer lastIndexOfParenthesis = inputJSON.toString().indexOf("}")
 		bodyJSON = inputJSON.toString().subSequence(indexOfParenthesis,inputJSON.toString().length())
-		//println "Salida body ${bodyJSON}"
-		bodyJSON
+		
+	}
+	
+	def adjustJSON(String inputJSON){
+		String bodyJSON = inputJSON.replaceAll("-","")
 	}
 	
 }
