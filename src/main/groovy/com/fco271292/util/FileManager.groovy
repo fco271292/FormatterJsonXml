0917931b6ef3b6110ec74d180280e9a524c5ed02
fco271292 <juan271292@hotmail.com>
2017-02-21 09:55:45 -0600

    Se agrega metodo para obtener el nombre del archivo, asi como ajustar el formato del codigo

--- src/main/groovy/com/fco271292/util/FileManager.groovy
+++ src/main/groovy/com/fco271292/util/FileManager.groovy
@@ -7,17 +7,36 @@ import groovy.transform.CompileStatic
 class FileManager {
 	
 	List<String> readFile(String filePath){
+		
 		def file = new File(filePath)
 		List contentFile = file.readLines()
-		println "Archivo ${contentFile}"
-		contentFile
+		
 	}
 	
-	File createFile(JSONCommand jsonCommand){
-		File fileJSON = new File("test.json")
+	File createFile(String filePath,JSONCommand jsonCommand){
+		
+		Map<String,String> fileName = generateFileName(filePath)
+		File fileJSON = new File("${fileName.name}.${fileName.fileFormat}")
 		fileJSON << jsonCommand.header
 		fileJSON << "\n"
 		fileJSON << new FormatterJSON().formatterJSONPretty(jsonCommand.body)
+		
+	}
+	
+	Map<String,String> generateFileName(String filePath){
+		
+		Map<String,String> map = new HashMap<String, String>()
+		File file = new File(filePath)
+		String fileNameFull = file.name
+		String name = fileNameFull.takeWhile {character->
+			character != "."
+		}
+		name = "${name}_${new Date().format("YYYY_MM_dd")}"
+		String fileFormat = fileNameFull.reverse().takeWhile {character->
+			character != "."
+		}
+		map << ["name": name,"fileFormat":fileFormat.reverse()]
+		
 	}
 	
 	
