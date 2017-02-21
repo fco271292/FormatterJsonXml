076900cf4867a807b81a99a2473d77fbe99d544d
fco271292 <juan271292@hotmail.com>
2017-02-21 09:54:45 -0600

    Se ajusta llamada de metodos

--- src/main/groovy/com/fco271292/app/App.groovy
+++ src/main/groovy/com/fco271292/app/App.groovy
@@ -13,17 +13,17 @@ class App {
 		File file = new File("")
 		
 		String pathFile = "${file.absolutePath}\\src\\test\\groovy\\source\\jsonTest.json"
-		//def fileTest = new File(pathFile)
+		
 		def contentFile =fileManager.readFile(pathFile)
 		String contentFileString = contentFile.join("\n")
 		String headerJSON = formatterJSON.getHeaderJSON(contentFileString)
 		String bodyJSON = formatterJSON.getbodyJSON(contentFileString)
+		def bodyJSONAdjust = formatterJSON.adjustJSON(bodyJSON)
 		//println "Header ${headerJSON} \n Body ${bodyJSON}"
-		def jsonCommand = new JSONCommand(header: headerJSON,body: bodyJSON )
-		new FileManager().createFile(jsonCommand)
+		def jsonCommand = new JSONCommand(header: headerJSON,body: bodyJSONAdjust )
+		//println "BODY ${bodyJSONAdjust}"
+		new FileManager().createFile(pathFile,jsonCommand)
 		println  "FIN..."
-		//new FormatterJSON().formatterJSONPretty(bodyJSON)
-		//new FormatterJSON().validateJSONInput(jsonInput)
 		
 	}
 
